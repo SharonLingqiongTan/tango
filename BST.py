8e8e816d31142f15c588002a036df299566fc92a
Lingqiong Tan <lingqiongtan@LingqiongdeMacBook.local>
2017-05-09 02:14:32 -0400

    Add BST

---
+++ BST.py
@@ -0,0 +1,8 @@
+class BST:
+	# def __init__(self):
+	def insert(self, key):
+		pass
+	def remove(self, key):
+		pass
+	def search(self, key):
+		pass
---
+++ tango.py
@@ -0,0 +1,15 @@
+from splay import SplayTree
+class TangoTree:
+	def __init__(self):
+		a = 1
+	def insert(self, key):
+		b= 2
+	def remove(self, key):
+		c = 3
+	# def traversal(self):
+
+	def search(self, key):
+		d = 3
+
+	def update(self):
+		f = 4
