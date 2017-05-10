e1d2f295d0235304002758d1360d949edc50d68e
Lingqiong Tan <lingqiongtan@LingqiongdeMacBook.local>
2017-05-10 01:24:47 -0400

    Update balanced tree

--- BST.py
+++ BST.py
@@ -1,8 +1,25 @@
-class BST:
-	# def __init__(self):
-	def insert(self, key):
-		pass
-	def remove(self, key):
-		pass
-	def search(self, key):
-		pass
+from treenode import TreeNode
+
+class BalancedTree:
+	def __init__(self, nodes):
+		self.root = self.initiate(nodes)
+	
+	def initiate(self, nodes):
+		if len(nodes) == 0:
+			return None
+		median = nodes[len(nodes)/2]
+		root = TreeNode(median)
+		if len(nodes) == 1:
+			return root
+		smaller = nodes[:len(nodes)/2]
+		larger = nodes[len(nodes)/2 + 1:]
+		left = self.initiate(smaller)
+		right = self.initiate(larger)
+		root.left = left
+		root.right = right
+		return root
+
+tree = BalancedTree([1,2,3,4,5,7,8])
+# print tree.root.key
+# print tree.root.left.key
+# print tree.root.left.right
